3e42710e20ab681ea06e7cd2e3771f6a027cb92b
Dachi Gogotchuri <dachi@geekytheory.com>
2015-07-17 20:55:40 +0200

    First Toolkit implementation. DelegateCommand, Converters, NavigationService and ViewModelBase with PageBase.

---
+++ GeekyTheory (UWP)/ViewModels/PageBase.cs
@@ -0,0 +1,37 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace GeekyTheory.ViewModels
+{
+    public class PageBase : Page
+    {
+        private ViewModelBase vm;
+
+        public PageBase()
+        {
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            vm = (ViewModelBase)this.DataContext;
+            if (vm != null)
+            {
+                vm.OnNavigatedTo(e);
+            }
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            vm.OnNavigatedFrom(e);
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            vm.OnNavigatingFrom(e);
+        }
+    }
+}
---
+++ GeekyTheory (UWP)/ViewModels/ViewModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml.Navigation;
+using GeekyTheory.Annotations;
+
+namespace GeekyTheory.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public virtual void OnNavigatedFrom(NavigationEventArgs e)
+        {
+        }
+
+        public virtual void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+        }
+
+        public virtual void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
