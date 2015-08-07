965464e240a33f9556626d120d2e66a574016c16
Dachi Gogotchuri <dachi@geekytheory.com>
2015-08-08 01:50:39 +0200

    ToUpperConverter added

---
+++ GeekyTheory (UWP)/Converters/ToUpperConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace GeekyTheory.Converters
+{
+    public class ToUpperConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString().ToUpperInvariant();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
