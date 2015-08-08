b1c254beace1fcb11c5d505b1a387fbf7ba5441b
Dachi Gogotchuri <dachi@geekytheory.com>
2015-08-08 20:34:41 +0200

    Updated PageBase and ViewModelBase acording to Windows10

--- GeekyTheory (UWP)/ViewModels/ViewModelBase.cs
+++ GeekyTheory (UWP)/ViewModels/ViewModelBase.cs
@@ -1,28 +1,55 @@
 ﻿using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace GeekyTheory.ViewModels
 {
-    public class ViewModelBase : INotifyPropertyChanged
+    public abstract class ViewModelBase : INotifyPropertyChanged
     {
-        public virtual void OnNavigatedFrom(NavigationEventArgs e)
+        private Frame appFrame;
+        private Frame splitViewFrame;
+        private bool isBusy;
+
+        public Frame AppFrame
         {
+            get { return appFrame; }
+        }
+        public Frame SplitViewFrame
+        {
+            get { return splitViewFrame; }
         }
 
-        public virtual void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        public bool IsBusy
         {
+            get { return isBusy; }
+            set
+            {
+                isBusy = value;
+                OnPropertyChanged();
+            }
         }
 
-        public virtual void OnNavigatedTo(NavigationEventArgs e)
-        {
-        }
+        public abstract Task OnNavigatedFrom(NavigationEventArgs e);
+
+        public abstract Task OnNavigatedTo(NavigationEventArgs e);
 
         public event PropertyChangedEventHandler PropertyChanged;
         
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        internal void SetAppFrame(Frame viewFrame)
+        {
+            appFrame = viewFrame;
+        }
+
+        internal void SetSplitFrame(Frame viewFrame)
+        {
+            splitViewFrame = viewFrame;
         }
     }
 }
